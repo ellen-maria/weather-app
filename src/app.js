8976fbefcdde901b90ee79a235600f3da7b3b7b3
ellen-maria <ellenmaria95@live.ie>
2021-10-25 18:56:21 +0100

    Updated JS using previous API

--- src/app.js
+++ src/app.js
@@ -43,7 +43,8 @@ function formatDate(timestamp) {
 let p1 = document.querySelector("#date");
 p1.innerHTML = formatDate(new Date());
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -66,6 +67,13 @@ function displayForecast() {
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "c5fda3433bf3c36bb1ab695d00f225b4";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function showTempertaure(response) {
@@ -97,6 +105,8 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
